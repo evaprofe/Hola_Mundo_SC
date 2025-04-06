a0c009aed16cc57bf44876a257143624596b85f8
evgoabprofe <evgoabprofe@gmail.com>
2025-04-06 17:02:05 +0200

    Modificaciones v3

--- Hola_Mundo_SC/src/hola_mundo_sc/Hola_Mundo_SC.java
+++ Hola_Mundo_SC/src/hola_mundo_sc/Hola_Mundo_SC.java
@@ -1,17 +1,23 @@
-
 package hola_mundo_sc;
 
-/**
- *
- * @author Evgoab
- */
-public class Hola_Mundo_SC {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+public class Hola_Mundo_SC extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML_HolaMundo_SC.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
