f6bbc7315a613353b1aa2249aed657d5119eada4
evgoabprofe <evgoabprofe@gmail.com>
2025-04-06 11:33:51 +0200

    primer commit

---
+++ Hola_Mundo_SB/src/hola_mundo_SB/FXML_HolaMundoController.java
@@ -0,0 +1,28 @@
+package hola_mundo_SB;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXML_HolaMundoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private Label lbTexto;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        lbTexto.setText("Hello World!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+// TODO
+    }
+}
