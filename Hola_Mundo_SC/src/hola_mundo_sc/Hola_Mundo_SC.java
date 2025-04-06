b00ae91888d3798608f8894defd2a4e4c23930e8
evgoabprofe <evgoabprofe@gmail.com>
2025-04-06 15:54:46 +0200

    Modificaciones v2

---
+++ Hola_Mundo_SC/src/hola_mundo_sc/Hola_Mundo_SC.java
@@ -0,0 +1,17 @@
+
+package hola_mundo_sc;
+
+/**
+ *
+ * @author Evgoab
+ */
+public class Hola_Mundo_SC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
